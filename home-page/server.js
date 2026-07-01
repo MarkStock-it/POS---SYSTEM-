e982078cf6936e452c6ce03161c797417b13d458
Mark Ely Salinas <elyandmarie@gmail.com>
2026-07-01 05:08:58 +0000

    Deployment fixes for render

--- home-page/server.js
+++ home-page/server.js
@@ -187,7 +187,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Simple PIN-based middleware for protected routes
 const POS_PIN = process.env.POS_PIN || '1234';
