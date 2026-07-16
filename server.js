06e494bfba6b8a52eb61094455c979bb9aa259ba
Mark Ely Salinas <elyandmarie@gmail.com>
2026-07-16 14:09:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { server } = require('./home-page/server.js');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 20243;
 
 server.listen(PORT, () => {
   console.log(`POS backend listening on http://localhost:${PORT}`);
